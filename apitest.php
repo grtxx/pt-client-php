c8621533820aec54a9d27fd47a83b0c65311979a
Gerebenics Andor <gerebenics.andor@umbrella.tv>
2025-06-11 23:27:25 +0000

    Remove unused video file from example files

---
+++ apitest.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+
+    include( __DIR__."/vendor/autoload.php" );
+
+
+    $pt = new \grt\pt\apiclient( "localhost:8000", "2396bb62-74d4-473d-8223-fb694fa91861", "d23a3280-04da-4c9e-a586-f45cadf8c68b" );
+    $pt->setProto( "http" );
+
+    $mgr = new \grt\pt\service\aimgr( $pt );
+
+    $task = $mgr->createTask( "ContentReviewTask", "aicontentreview", [] );
+    if ( $task === false ) {
+        print( "Error creating task\n" );
+        exit( 1 );
+    }
+    $task->addFile( 'media', __DIR__."/examplefiles/video.mp4" );
+    $task->enqueue();
+    while ( $task->getStatus() != "ok" && $task->getStatus() != "error" ) {
+        print( "Task is still running, waiting...\n" );
+        sleep( 1 );
+        $task->refresh();
+    }
+    
+    print_r( $task->getStatus() );
+    print_r( $task->getResult() );
+
+    print( "END\n" );
